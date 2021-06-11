c733566f2f7141c6cbd5b1aca7bcf0f3b03cef76
Barış <85716242+bariscodefxy@users.noreply.github.com>
2021-06-11 15:17:33 +0300

    __construct() function update
    trying database connect.

--- src/bariscodefx/BasicDB/BasicDB.php
+++ src/bariscodefx/BasicDB/BasicDB.php
@@ -2,6 +2,26 @@
 
 namespace bariscodefx\BasicDB;
 
-class BasicDB {
-  
+class BasicDB extends \PDO {
+    
+    public function __construct($host, $dbname, $username, $password, $charset = "utf-8"){
+        if(!$host)
+        {
+            Throw new \Exception("Host must be defined");
+        }else if(!$dbname) {
+            Throw new \Exception("Database must be defined");
+        }else if(!$username) {
+            Throw new \Exception("Username must be defined");
+        }else if(!$password) {
+            Throw new \Exception("Password must be defined");
+        }
+        
+        try{
+            $this->parent::__construct("mysql:host=$host;db=$dbname;charset=$charset", $username, $password);
+        }catch(PDOException $e)
+        {
+            Throw new $e;
+        }
+    }
+    
 }
