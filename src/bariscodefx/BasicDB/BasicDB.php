530bbdb179a0ff4f2f218f87c0e0da121379bc68
Barış <85716242+bariscodefxy@users.noreply.github.com>
2021-06-11 15:42:04 +0300

    Edit

--- src/bariscodefx/BasicDB/BasicDB.php
+++ src/bariscodefx/BasicDB/BasicDB.php
@@ -5,17 +5,6 @@ namespace bariscodefx\BasicDB;
 class BasicDB extends \PDO {
     
     public function __construct($host, $dbname, $username, $password, $charset = "utf-8"){
-        if(!$host)
-        {
-            Throw new \Exception("Host must be defined");
-        }else if(!$dbname) {
-            Throw new \Exception("Database must be defined");
-        }else if(!$username) {
-            Throw new \Exception("Username must be defined");
-        }else if(!$password) {
-            Throw new \Exception("Password must be defined");
-        }
-        
         try{
             parent::__construct("mysql:host='". $host ."';dbname='". $dbname ."';charset='". $charset ."'", $username, $password);
         }catch(PDOException $e)
