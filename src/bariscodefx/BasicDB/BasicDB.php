2c06b9a65514b1443a5c16156284ecef3c7fcaba
Barış <85716242+bariscodefxy@users.noreply.github.com>
2021-06-11 16:01:36 +0300

    Connection Test

--- src/bariscodefx/BasicDB/BasicDB.php
+++ src/bariscodefx/BasicDB/BasicDB.php
@@ -13,7 +13,7 @@ class BasicDB extends \PDO {
     */
     public function __construct($host, $dbname, $username, $password, $charset = "utf-8"){
         try{
-            parent::__construct("mysql:host='". $host ."';dbname='". $dbname ."';charset='". $charset ."'", $username, $password);
+            parent::__construct("mysql:host=". $host .";dbname=". $dbname .";charset=". $charset . "\", $username, $password);
         }catch(PDOException $e)
         {
             echo $e->getMessage() . PHP_EOL;
