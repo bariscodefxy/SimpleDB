62d200f6d6e68604de42d16281213ca0bb694d29
Barış <85716242+bariscodefxy@users.noreply.github.com>
2021-06-11 15:51:18 +0300

    add comments

--- src/bariscodefx/BasicDB/BasicDB.php
+++ src/bariscodefx/BasicDB/BasicDB.php
@@ -2,8 +2,15 @@
 
 namespace bariscodefx\BasicDB;
 
+/**
+* Class BasicDB
+*/
 class BasicDB extends \PDO {
     
+    /**
+    * Constructor of Class
+    * Base of Connection
+    */
     public function __construct($host, $dbname, $username, $password, $charset = "utf-8"){
         try{
             parent::__construct("mysql:host='". $host ."';dbname='". $dbname ."';charset='". $charset ."'", $username, $password);
