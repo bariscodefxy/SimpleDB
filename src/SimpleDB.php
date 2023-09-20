fc24068efcd87d8123f9f08379d034b4a11aa2ba
bariscodefx <bariscodefx@gmail.com>
2023-09-21 00:24:41 +0300

    update src/SimpleDB.php

---
+++ src/SimpleDB.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SimpleDB;
+
+use PDO;
+
+/**
+* Class SimpleDB
+*/
+class SimpleDB extends PDO {
+    
+    /**
+    * Constructor of Class
+    * Base of Connection
+    */
+    public function __construct($host, $dbname, $username, $password, $charset = "utf8"){
+        try{
+            parent::__construct("mysql:host=". $host .";dbname=". $dbname .";charset=". $charset, $username, $password);
+        }catch(\PDOException $e)
+        {
+            echo $e->getMessage() . PHP_EOL;
+        }
+    }
+    
+}
